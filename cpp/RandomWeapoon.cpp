f05904c5fa118c25f8dd0d6863c928d13368d738
OlegNotFound38 <pronkinoa@gmail.com>
2026-04-23 18:37:46 +0300

    Первая работающая верся программы, выводящая цифры

--- cpp/RandomWeapoon.cpp
+++ cpp/RandomWeapoon.cpp
@@ -7,19 +7,26 @@ using namespace std;
 int main(){
     setlocale(LC_ALL, "ru_RU.UTF-8");
     cout << "Привет! Это рандомайзер для оружек в кс. Работает так:\nВ игре нажимаешь \"B\" (закупиться), а дальше по очереди цифры, которые выдаст рандомайзер. "<<endl;
-    cout << "Для начала работы введи в терминал, все что угодно,\nЧтобы остановить программу напиши \"Stop\"" << endl;
+    cout << "Для начала работы введи в терминал ченибудь,\nЧтобы остановить программу напиши \"Stop\"" << endl;
     cout << "Удачи!" << endl;
 
     
     string StopCheaker;
     cin >> StopCheaker;
 
-    while (StopCheaker != "Stop"){
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<> dist(1, 10);
+    while (!(StopCheaker == "Stop" || StopCheaker == "stop")){
+        system("cls");
+        mt19937 gen(random_device{}());
+        uniform_int_distribution<> WPcolumn(2, 4);
+        uniform_int_distribution<> WPstring(1, 5);
+
+        cout << WPcolumn(gen) << endl << WPstring(gen) << endl;
+
+        cin >> StopCheaker;
 
     }
+
+    cout << "Удачи!" << endl;
 
     return 0;
 }
