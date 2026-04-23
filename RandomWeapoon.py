7905c24f33a1b87c38dba7686676170ccd8eb652
OlegNotFound38 <pronkinoa@gmail.com>
2026-04-23 17:36:04 +0300

    Add posible to stop

--- RandomWeapoon.py
+++ RandomWeapoon.py
@@ -1,5 +1,9 @@
 import random
-while True:
-    input()
+
+StopCheaker = ''
+
+while StopCheaker != 'Stop':
+    StopCheaker = input()
+    
     print(random.randint(2,4))
     print(random.randint(1,5))
