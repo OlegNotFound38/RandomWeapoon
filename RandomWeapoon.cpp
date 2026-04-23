d6dd12e7bfd897f620165d0756a341c24889da5b
OlegNotFound38 <pronkinoa@gmail.com>
2026-04-23 18:14:55 +0300

    Добавил цикл и остановку

--- RandomWeapoon.cpp
+++ RandomWeapoon.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <random>
+#include <string>
 
 using namespace std;
 
 int main(){
+    setlocale(LC_ALL, "Rus");
     cout << "Привет! Это рандомайзер для оружек в кс. Работает так:\n В игре нажимаешь \"B\" (закупиться), а дальше по очереди цифры, которые выдаст рандомайзер. "<<endl;
     cout << "Для начала работы введи в терминал, все что угодно,\nЧтобы остановить программу напиши \"Stop\"" << endl;
     cout << "Удачи!" << endl;
 
-    // std::random_device rd;
-    // std::mt19937 gen(rd());
-    // std::uniform_int_distribution<> dist(1, 10);
+    
+    string StopCheaker;
+    cin >> StopCheaker;
+
+    while (StopCheaker != "Stop"){
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dist(1, 10);
+
+    }
 
     return 0;
 }
